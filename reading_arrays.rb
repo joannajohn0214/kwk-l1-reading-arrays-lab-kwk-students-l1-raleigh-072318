67fbebe133e01479a9b4f244f90ec684c25dc05d
joannajohn0214 <joannajohn0214@gmail.com>
2018-07-25 18:23:34 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,41 +1,38 @@
-student_names = []
-  student_names[0]="Adele"
-  student_names[1]="Beyoncé"
-  student_names[2]="Cardi B"
-  student_names[3]="Lady Gaga"
-  student_names[4]="Nicki Minaj"
-  student_names[5]="Rihanna"
+student_names = [
+  "Adele"
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
 
+def first_student_by_index
+  # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[0]
+end
 
-def first_student_by_index(student_names)
+def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts "#{student_names[0]}"
+  STUDENT_NAMES[3]
 end
-first_student_by_index(student_names)
-def fourth_student_by_index(student_names)
+
+def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts "#{student_names[3]}"
+  STUDENT_NAMES[5]
 end
-fourth_student_by_index(student_names)
-def last_student_by_index(student_names)
-  # Write a solution that returns the first student in the array student_names
-  puts "#{student_names[5]}"
-end
-last_student_by_index(student_names)
-def first_student_by_method(student_names)
+
+def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-  puts "#{student_names.first}"
+  STUDENT_NAMES.first
 end
-first_student_by_method(student_names)
-def last_student_by_method(student_names)
+
+def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
-  puts "#{student_names.last}"
+  STUDENT_NAMES.last
 end
-last_student_by_method(student_names)
-def first_second_and_third_students(student_names)
+
+def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  puts "#{student_names[0]}"
-  puts "#{student_names[1]}"
-  puts "#{student_names[2]}"
+  STUDENT_NAMES[0..2]
 end
-first_second_and_third_students(student_names)
